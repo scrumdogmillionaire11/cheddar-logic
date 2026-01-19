299871a16668e9c6ba1cb6c2a9d608669bea154d
AJ <aj.colub@gmail.com>
2026-01-19 14:14:34 -0500

    remove unused code

--- web/src/components/marketing/index.ts
+++
@@ -1,7 +0,0 @@
-export { HeroPanel } from "./HeroPanel";
-export { MethodologyShowcase } from "./MethodologyShowcase";
-export { DualServiceSplit } from "./DualServiceSplit";
-export { DiscordCTA } from "./DiscordCTA";
-export { ContactCard } from "./ContactCard";
-export { ComplianceFooter } from "./ComplianceFooter";
-export { KillSwitchBanner } from "./KillSwitchBanner";
