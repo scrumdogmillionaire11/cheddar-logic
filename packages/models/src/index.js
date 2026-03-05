2e2b7fb972b82eabce170d45b429063802bb6ad7
AJ <aj.colub@gmail.com>
2026-03-04 19:45:59 -0500

    chore(models): export consolidated card utilities
    - Export computeWinProbHome and buildDriverSummary from @cheddar-logic/models
    - Enables imports: const { computeWinProbHome } = require('@cheddar-logic/models')

--- packages/models/src/index.js
+++ packages/models/src/index.js
@@ -11,3 +11,8 @@ module.exports = {
 	...decisionGate,
 	...nhlSog
 };
+
+// Consolidated card utilities (2026-03-04)
+const { computeWinProbHome, buildDriverSummary } = require('./card-utilities');
+module.exports.computeWinProbHome = computeWinProbHome;
+module.exports.buildDriverSummary = buildDriverSummary;
