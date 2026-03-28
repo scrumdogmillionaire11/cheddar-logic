c7eb34e3351fc1f1479cdcda70473f09cbfe7b55
AJ <aj.colub@gmail.com>
2026-03-28 07:03:34 -0400

    deploy fix

--- web/src/app/fpl/page.tsx
+++ web/src/app/fpl/page.tsx
@@ -1,11 +1,10 @@
-import { closeDatabaseReadOnly, initDb } from '@cheddar-logic/data';
+import { closeDatabaseReadOnly } from '@cheddar-logic/data';
 import FPLPageClient from '@/components/fpl-page-client';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
 export default async function FPLPage() {
-  await initDb();
   try {
     // AUTH DISABLED: Commenting out auth walls to allow public access
     // const auth = getAccessTokenAuthResult(accessToken, RESOURCE.FPL_SAGE);
