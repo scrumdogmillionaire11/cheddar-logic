98e3a200c9a0f8a09484bb6b65754e605e42524b
AJ <aj.colub@gmail.com>
2026-03-30 13:00:01 -0400

    658

--- cheddar-fpl-sage/src/cheddar_fpl_sage/analysis/__init__.py
+++ cheddar-fpl-sage/src/cheddar_fpl_sage/analysis/__init__.py
@@ -5,5 +5,6 @@ Enhanced decision framework and integrations
 
 from .enhanced_decision_framework import EnhancedDecisionFramework
 from .fpl_sage_integration import FPLSageIntegration
+from . import draft_builder
 
-__all__ = ['EnhancedDecisionFramework', 'FPLSageIntegration']
+__all__ = ['EnhancedDecisionFramework', 'FPLSageIntegration', 'draft_builder']
